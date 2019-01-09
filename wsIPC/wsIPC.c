0e2d95778446561f07df090c6c99d93eb27b099b
David Kaplan (DEPLETIONMODE) <dakapl@microsoft.com>
2019-01-09 17:58:07 +0000

    Remove unnecessary header

--- wsIPC/wsIPC.c
+++ wsIPC/wsIPC.c
@@ -2,7 +2,21 @@
 // @depletionmode 2018
 //
 
-#include "wsIPC.h"
+#include <Windows.h>
+
+__declspec(dllexport)
+HRESULT Send(
+	_In_reads_bytes_(BufferSize) PBYTE Buffer,
+	_In_ ULONG BufferSize
+);
+
+__declspec(dllexport)
+HRESULT Receive(
+	_Out_writes_bytes_(*BytesReceived) BYTE* Buffer,
+	_In_ SIZE_T BufferSize,
+	_Out_ SIZE_T *BytesReceived
+);
+
 
 #include <Psapi.h>
 
